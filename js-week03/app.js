7669af392bf0f186cc829e84a65604385e91cebf
AngusWu <anguswu@simbo.com.tw>
2020-07-05 15:57:13 +0800

    when open productModal show '新增產品' title

--- js-week03/app.js
+++ js-week03/app.js
@@ -7,6 +7,8 @@ new Vue({
   methods: {
     openModal(action) {
       console.log('action', action);
+      document.querySelector('#productLabel > span').innerHTML = '新增產品';
+      $('#productModal').modal('show');
     }
   }
 });
