0814ad1cd163f8e8b91dd51ded3481197bcc64d7
AngusWu <anguswu@simbo.com.tw>
2020-07-12 22:30:13 +0800

    feat: apply product page

--- js-week04/product.js
+++ js-week04/product.js
@@ -1,7 +1,67 @@
 new Vue({
   el: '#app',
-  data: {},
-  methods: {},
+  data: {
+    products: [],
+    tempProduct: {},
+  },
+  methods: {
+    retrieveStars () {
+      return ((this.tempProduct.options || {}).stars || 0);
+    },
+    starComment (event) {
+      const star = parseInt(event.target.id.split('-')[1], 10);
+      for (let i = 1; i <= 5; i++) {
+        document.getElementById('star-' + i).classList.remove('checked');
+      }
+      for (let i = 1; i <= event.target.id.split('-')[1]; i++) {
+        document.getElementById('star-' + i).classList.add('checked');
+      }
+      this.tempProduct = Object.assign({}, this.tempProduct, { options: { stars: star } });
+    },
+    deleteProduct () {
+      const index = this.products
+        .findIndex(item => item.id === this.tempProduct.id);
+
+      if (index !== -1) this.products.splice(index, 1);
+
+      this.tempProduct = {};
+      controlDeleteProductModal(false);
+    },
+    updateProduct () {
+      if (Object.keys(this.tempProduct).length <= 0) return;
+
+      const index = this.products
+        .findIndex(item => item.id === this.tempProduct.id);
+
+      if (index !== -1) {
+        this.$set(this.products, index, JSON.parse(JSON.stringify(this.tempProduct)));
+      } else {
+        this.tempProduct.id = _uuid();
+        this.products.splice(this.products.length, 0, JSON.parse(JSON.stringify(this.tempProduct)));
+      }
+
+      controlProductModal(false);
+      this.tempProduct = {};
+    },
+    openModal (action, item = {}) {
+      this.tempProduct = JSON.parse(JSON.stringify(item));
+      switch (action) {
+        case 'create':
+          changeProductModalTitle('新增產品');
+          controlProductModal(true);
+          break;
+        case 'edit':
+          changeProductModalTitle('編輯產品');
+          controlProductModal(true);
+          break;
+        case 'delete':
+          controlDeleteProductModal(true);
+          break;
+        default:
+          break;
+      }
+    }
+  },
   beforeCreate () {
     if (!isCookieExists('token')) window.location = 'login.html';
   }
