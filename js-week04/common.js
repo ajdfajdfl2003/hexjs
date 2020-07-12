9287d0f8daa5dd42586700cd16032170d395aa67
AngusWu <anguswu@simbo.com.tw>
2020-07-13 00:19:16 +0800

    feat: can get product list (first page)

--- js-week04/common.js
+++ js-week04/common.js
@@ -2,3 +2,10 @@ function isCookieExists (name) {
   return document.cookie.split(';')
     .some((item) => item.trim().startsWith(`${name}=`))
 }
+
+function retrieveCookie (name) {
+  return document.cookie
+    .split('; ')
+    .find(row => row.startsWith(name))
+    .split('=')[1];
+}
--- js-week04/product.js
+++ js-week04/product.js
@@ -1,11 +1,25 @@
 new Vue({
   el: '#app',
   data: {
+    user: { uuid: '19831eca-3ff8-4cbe-9167-80b24e16783f', token: '' },
     products: [],
-    tempProduct: {},
   },
-  methods: {},
-  beforeCreate () {
+  methods: {
+    showProducts (page = 0) {
+      const apiUrl = `https://course-ec-api.hexschool.io/api/${this.user.uuid}/admin/ec/products`;
+      axios.get(apiUrl, {
+        headers: { 'authorization': `Bearer ${this.user.token}` },
+        params: { page }
+      }).then(({ data: { data, meta } }) => {
+        this.products = data;
+      }).catch(error => {
+        console.log(error);
+      });
+    }
+  },
+  created () {
     if (!isCookieExists('token')) window.location = 'login.html';
+    this.user.token = retrieveCookie('token');
+    this.showProducts();
   }
 });
