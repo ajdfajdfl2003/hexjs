6c9bb4df67d97e6a06c336661c18a7595835b069
AngusWu <anguswu@simbo.com.tw>
2020-07-19 17:02:41 +0800

    feat: separate main page and product list page

---
+++ js-week05/components/product-list.js
@@ -0,0 +1,141 @@
+Vue.component('productList', {
+  template: ` <div id="product-list" class="row my-4">
+        <div class="col-12 col-lg-3 mb-4">
+            <div class="card bg-light">
+                <div class="card-header bg-info text-white text-uppercase p-3"><i class="fa fa-list"></i> Categories
+                </div>
+                <ul class="list-group category-block">
+                    <li class="list-group-item"><a href="#">Cras justo odio</a></li>
+                    <li class="list-group-item"><a href="#">Dapibus ac facilisis in</a></li>
+                    <li class="list-group-item"><a href="#">Morbi leo risus</a></li>
+                    <li class="list-group-item"><a href="#">Porta ac consectetur ac</a></li>
+                    <li class="list-group-item"><a href="#">Vestibulum at eros</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-12 col-lg-9">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-6 mb-2">
+                    <div class="card">
+                        <img class="card-img-top"
+                             src="https://dummyimage.com/600x400/55595c/fff"
+                             alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title text-success">Product title</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <div class="d-flex justify-content-between align-items-baseline"><!---->
+                                <del class="h6 text-secondary">原價 20000 元</del>
+                                <div class="h5 text-dark">現在只要 20000 元</div>
+                            </div>
+                        </div>
+                        <div class="card-footer d-flex">
+                            <button type="button" class="btn btn-light">More</button>
+                            <button type="button" class="btn btn-info ml-auto">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-6 mb-2">
+                    <div class="card">
+                        <img class="card-img-top"
+                             src="https://dummyimage.com/600x400/55595c/fff"
+                             alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title text-success">Product title</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <div class="d-flex justify-content-between align-items-baseline"><!---->
+                                <del class="h6 text-secondary">原價 20000 元</del>
+                                <div class="h5 text-dark">現在只要 20000 元</div>
+                            </div>
+                        </div>
+                        <div class="card-footer d-flex">
+                            <button type="button" class="btn btn-light">More</button>
+                            <button type="button" class="btn btn-info ml-auto">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-6 mb-2">
+                    <div class="card">
+                        <img class="card-img-top"
+                             src="https://dummyimage.com/600x400/55595c/fff"
+                             alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title text-success">Product title</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <div class="d-flex justify-content-between align-items-baseline"><!---->
+                                <del class="h6 text-secondary">原價 20000 元</del>
+                                <div class="h5 text-dark">現在只要 20000 元</div>
+                            </div>
+                        </div>
+                        <div class="card-footer d-flex">
+                            <button type="button" class="btn btn-light">More</button>
+                            <button type="button" class="btn btn-info ml-auto">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-6 mb-2">
+                    <div class="card">
+                        <img class="card-img-top"
+                             src="https://dummyimage.com/600x400/55595c/fff"
+                             alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title text-success">Product title</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <div class="d-flex justify-content-between align-items-baseline"><!---->
+                                <del class="h6 text-secondary">原價 20000 元</del>
+                                <div class="h5 text-dark">現在只要 20000 元</div>
+                            </div>
+                        </div>
+                        <div class="card-footer d-flex">
+                            <button type="button" class="btn btn-light">More</button>
+                            <button type="button" class="btn btn-info ml-auto">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-6 mb-2">
+                    <div class="card">
+                        <img class="card-img-top"
+                             src="https://dummyimage.com/600x400/55595c/fff"
+                             alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title text-success">Product title</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <div class="d-flex justify-content-between align-items-baseline"><!---->
+                                <del class="h6 text-secondary">原價 20000 元</del>
+                                <div class="h5 text-dark">現在只要 20000 元</div>
+                            </div>
+                        </div>
+                        <div class="card-footer d-flex">
+                            <button type="button" class="btn btn-light">More</button>
+                            <button type="button" class="btn btn-info ml-auto">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-6 mb-2">
+                    <div class="card">
+                        <img class="card-img-top"
+                             src="https://dummyimage.com/600x400/55595c/fff"
+                             alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title text-success">Product title</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <div class="d-flex justify-content-between align-items-baseline"><!---->
+                                <del class="h6 text-secondary">原價 20000 元</del>
+                                <div class="h5 text-dark">現在只要 20000 元</div>
+                            </div>
+                        </div>
+                        <div class="card-footer d-flex">
+                            <button type="button" class="btn btn-light">More</button>
+                            <button type="button" class="btn btn-info ml-auto">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`
+})
