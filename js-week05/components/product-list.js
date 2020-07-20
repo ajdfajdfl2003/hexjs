e48124b34847fab9bf1f6e655723528d360289dc
AngusWu <anguswu@simbo.com.tw>
2020-07-21 01:10:04 +0800

    feat: apply category

--- js-week05/components/product-list.js
+++ js-week05/components/product-list.js
@@ -7,8 +7,9 @@ Vue.component('productList', {
                     <span id="category">Categories</span>
                 </div>
                 <ul class="list-group category-block">
-                    <li v-for="item in categories" :key="item" 
-                        class="list-group-item">
+                    <li v-for="item in categories" :key="item"
+                        @click.prevent="applyCategory(item)"
+                        class="list-group-item" :class="{ active: currentCategory === item }">
                         <a href="#">{{ item }}</a>
                     </li>
                 </ul>
@@ -16,7 +17,7 @@ Vue.component('productList', {
         </div>
         <div class="col-12 col-lg-10">
             <div class="row">
-                <div v-for="product in products" :key="product.id"
+                <div v-for="product in filterProducts" :key="product.id"
                     class="col-12 col-md-6 col-lg-4 mb-2">
                     <div class="card">
                         <img class="card-img-top"
@@ -43,10 +44,18 @@ Vue.component('productList', {
   data () {
     return {
       categories: [],
+      currentCategory: '',
       products: []
     }
   },
   methods: {
+    applyCategory (item) {
+      if (this.currentCategory === '') {
+        this.currentCategory = item;
+      } else {
+        this.currentCategory = '';
+      }
+    },
     retrieveProducts (page = 0) {
       axios.get(`${apiUrlPrefix}/products`, {
         params: { page }
@@ -54,9 +63,15 @@ Vue.component('productList', {
         this.categories = new Set(data.map(product => product.category))
         this.products = data;
       });
-    },
+    }
   },
   created () {
     this.retrieveProducts();
+  },
+  computed: {
+    filterProducts () {
+      if (this.currentCategory === '') return this.products;
+      return this.products.filter(item => this.currentCategory === item.category);
+    }
   }
 });
