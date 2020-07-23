8c4ff0f6eedbbb29bd85a09104c528c2efd734d8
AngusWu <anguswu@simbo.com.tw>
2020-07-23 13:45:09 +0800

    feat: apply form validate with VeeValidate

---
+++ js-week05/components/shipping-info.js
@@ -0,0 +1,67 @@
+Vue.component('shippingInfo', {
+  template: `
+    <div class="my-5 row justify-content-center">
+        <validation-observer v-slot="{ invalid }" class="col-md-6">
+            <form @submit.prevent="">
+                <validation-provider rules="required" class="form-group" tag="div"
+                                     v-slot="{ errors, classes }">
+                    <label for="name">收件人姓名</label>
+                    <input id="name" type="text" name="收件人姓名" class="form-control" :class="classes" v-model="shipper.name">
+                    <span class="invalid-feedback">{{ errors[0] }}</span>
+                </validation-provider>
+                <validation-provider rules="required|email" class="form-group" tag="div"
+                                     v-slot="{ errors, classes }">
+                    <label for="email">Email</label>
+                    <input id="email" type="email" name="Email" class="form-control" :class="classes" v-model="shipper.email">
+                    <span class="invalid-feedback">{{ errors[0] }}</span>
+                </validation-provider>
+                <validation-provider rules="required|min:8" class="form-group" tag="div"
+                                     v-slot="{ errors, classes }">
+                    <label for="tel">電話</label>
+                    <input id="tel" type="tel" name="電話" class="form-control" :class="classes" v-model="shipper.tel">
+                    <span class="invalid-feedback">{{ errors[0] }}</span>
+                </validation-provider>
+                <validation-provider rules="required" class="form-group" tag="div"
+                                     v-slot="{ errors, classes }">
+                    <label for="address">地址</label>
+                    <input id="address" type="text" name="地址" class="form-control" :class="classes" v-model="shipper.address">
+                    <span class="invalid-feedback">{{ errors[0] }}</span>
+                </validation-provider>
+                <validation-provider rules="required" class="form-group" tag="div"
+                                     v-slot="{ errors, classes }">
+                    <label for="payment">付款方式</label>
+                    <select id="payment" name="付款方式" class="form-control" :class="classes" v-model="shipper.payment">
+                        <option value="" disabled selected>請選擇付款方式</option>
+                        <option value="WebATM">WebATM</option>
+                        <option value="ATM">ATM</option>
+                        <option value="Barcode">Barcode</option>
+                        <option value="Credit">Credit</option>
+                        <option value="ApplePay">ApplePay</option>
+                        <option value="GooglePay">GooglePay</option>
+                    </select>
+                    <span class="invalid-feedback">{{ errors[0] }}</span>
+                </validation-provider>
+                <div class="form-group">
+                    <label for="message">留言</label>
+                    <textarea id="message" cols="30" rows="3" class="form-control"></textarea>
+                </div>
+                <div class="text-right">
+                    <button type="submit" class="btn btn-info" :disabled="invalid">送出表單</button>
+                </div>
+            </form>
+        </validation-observer>
+    </div>  
+  `,
+  data () {
+    return {
+      shipper: {
+        name: '',
+        email: '',
+        tel: '',
+        address: '',
+        payment: '',
+        comment: ''
+      }
+    }
+  }
+});
--- js-week05/index.js
+++ js-week05/index.js
@@ -1,7 +1,18 @@
 const apiUrlPrefix = 'https://course-ec-api.hexschool.io/api/19831eca-3ff8-4cbe-9167-80b24e16783f/ec';
 
-Vue.use(VueLoading);
 Vue.prototype.$bus = new Vue();
+
+Vue.use(VueLoading);
+
+Vue.component('ValidationObserver', VeeValidate.ValidationObserver);
+Vue.component('ValidationProvider', VeeValidate.ValidationProvider);
+
+VeeValidate.configure({
+  classes: {
+    valid: 'is-valid',
+    invalid: 'is-invalid',
+  }
+});
 
 new Vue({
   el: '#app',
