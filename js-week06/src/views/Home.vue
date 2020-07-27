a155e50054d69c460e286c14cbf85c28e58640ea
Angus <ajdfajdfl2003@gmail.com>
2020-07-27 23:07:16 +0800

    feat: apply bootstrap navbar

--- js-week06/src/views/Home.vue
+++ js-week06/src/views/Home.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="home">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
       <button class="navbar-toggler" type="button" data-toggle="collapse"
               data-target="#navbarHome" aria-controls="navbarHome"
               aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarHome">
-        <router-link to="/" class="nav-link">首頁</router-link>
-        <router-link to="/product/123" class="nav-link">產品頁面</router-link>
-        <router-link to="/cart" class="nav-link">購物車</router-link>
-        <router-link to="/payment" class="nav-link">結帳</router-link>
-        <router-link to="/payment/finish" class="nav-link">結帳完成</router-link>
-        <router-link to="/about" class="nav-link">關於我們</router-link>
-        <router-link to="/dashboard" class="nav-link">後台 without login</router-link>
+        <div class="navbar-nav">
+          <router-link to="/" class="nav-item nav-link">首頁</router-link>
+            <router-link to="/product/123" class="nav-item nav-link">產品頁面</router-link>
+            <router-link to="/cart" class="nav-item nav-link">購物車</router-link>
+            <router-link to="/payment" class="nav-item nav-link">結帳</router-link>
+            <router-link to="/payment/finish" class="nav-item nav-link">結帳完成</router-link>
+            <router-link to="/about" class="nav-item nav-link">關於我們</router-link>
+            <router-link to="/dashboard" class="nav-item nav-link">後台 without login</router-link>
+        </div>
       </div>
     </nav>
     <router-view/>
@@ -22,9 +24,16 @@
 
 <script>
 
+import utils from './mixins/utils';
+
 export default {
   name: 'Home',
-  components: {
+  mixins: [utils],
+  mounted() {
+    this.handleAddActive();
+  },
+  updated() {
+    this.handleAddActive();
   },
 };
 </script>
--- js-week06/src/views/dashboard/Dashboard.vue
+++ js-week06/src/views/dashboard/Dashboard.vue
@@ -1,20 +1,38 @@
 <template>
    <div class="dashboard">
-     <div class="nav">
-       <router-link to="/dashboard">後台首頁</router-link> |
-       <router-link to="/dashboard/coupons">優惠券列表</router-link> |
-       <router-link to="/dashboard/order-list">訂單列表</router-link> |
-       <router-link to="/dashboard/photos">圖片儲存列表</router-link> |
-       <router-link to="/">回到前台</router-link> |
-       <router-link to="/dashboard/logout">登出</router-link>
-     </div>
+     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+       <button class="navbar-toggler" type="button" data-toggle="collapse"
+               data-target="#navbarDashboard" aria-controls="navbarDashboard"
+               aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+       </button>
+       <div class="collapse navbar-collapse" id="navbarDashboard">
+         <div class="navbar-nav">
+           <router-link to="/dashboard" class="nav-item nav-link">後台首頁</router-link>
+           <router-link to="/dashboard/coupons" class="nav-item nav-link">優惠券列表</router-link>
+           <router-link to="/dashboard/order-list" class="nav-item nav-link">訂單列表</router-link>
+           <router-link to="/dashboard/photos" class="nav-item nav-link">圖片儲存列表</router-link>
+           <router-link to="/" class="nav-item nav-link">回到前台</router-link>
+           <router-link to="/dashboard/logout" class="nav-item nav-link">登出</router-link>
+         </div>
+       </div>
+     </nav>
      <router-view></router-view>
    </div>
 </template>
 
 <script>
+import utils from '../mixins/utils';
+
 export default {
   name: 'dashboard',
+  mixins: [utils],
+  mounted() {
+    this.handleAddActive();
+  },
+  updated() {
+    this.handleAddActive();
+  },
 };
 </script>
 
