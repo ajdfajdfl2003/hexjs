516c0a3fcd276fca0075bb5e1f0cebee1153081c
AngusWu <anguswu@simbo.com.tw>
2020-07-29 13:15:56 +0800

    unfinish: apply week05 frontend product page with api

--- js-week06/src/main.js
+++ js-week06/src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 import App from './App.vue';
 import router from './router';
-import 'jquery';
-import 'bootstrap';
+
+window.$ = require('jquery');
+
+Vue.use(VueAxios, axios);
+Vue.use(Loading);
 
 Vue.config.productionTip = false;
 
--- js-week06/src/views/home/Products.vue
+++ js-week06/src/views/home/Products.vue
@@ -1,6 +1,174 @@
 <template>
-  <div class="products">
-    <h1>These are home page and product list.</h1>
-    <img src="../../assets/img/working.jpg" style="max-width: 100%">
+  <div id="products" class="row my-4">
+    <div class="col-12 col-lg-2 mb-4">
+      <div class="card bg-light">
+        <div class="card-header bg-info text-white text-uppercase p-3">
+          <i class="fas fa-list"></i>
+        </div>
+        <ul class="list-group category-block">
+          <li v-for="item in categories" :key="item"
+              @click.prevent="applyCategory(item)"
+              class="list-group-item" :class="{ active: currentCategory === item }">
+            <a href="#">{{ item }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-12 col-lg-10">
+      <div class="row">
+        <div v-for="product in filterProducts" :key="product.id"
+             class="col-12 col-md-6 col-lg-4 mb-2">
+          <div class="card">
+            <img class="card-img-top"
+                 :src="product.imageUrl[0]"
+                 :alt="product.title">
+            <div class="card-body">
+              <span class="badge badge-pill badge-secondary mb-2">{{ product.category }}</span>
+              <h5 class="card-title text-success">{{ product.title }}</h5>
+              <p class="card-text">{{ product.content }}</p>
+              <div class="d-flex justify-content-between align-items-baseline">
+                <del class="h6 text-secondary">原價 {{ product.origin_price }} 元</del>
+                <div class="h5 text-dark">現在只要 {{ product.price }} 元</div>
+              </div>
+            </div>
+            <div class="card-footer d-flex">
+              <div style="position: relative" :ref="'moreDetail'+product.id">
+                <button type="button" class="btn btn-light" @click.prevent="openModal(product.id)">
+                  More
+                </button>
+              </div>
+              <button type="button" class="btn btn-info ml-auto"
+                      @click.prevent="addToCart(product)">Add to cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="modal fade" ref="moreModal" tabindex="-1" role="dialog"
+         aria-labelledby="moreModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" ref="moreModalLabel">{{ tempProduct.title }}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <blockquote class="blockquote">
+              <p class="mb-0">{{ tempProduct.content }}</p>
+              <p class="mb-1 text-muted"><small>{{ tempProduct.description }}</small></p>
+            </blockquote>
+            <div>
+              <div class="d-flex justify-content-between align-items-baseline">
+                <del class="h6 text-secondary">原價 {{ tempProduct.origin_price }} 元</del>
+                <div class="h5 text-dark">現在只要 {{ tempProduct.price }} 元</div>
+              </div>
+            </div>
+            <select class="form-control mt-3" v-model="tempProduct.count">
+              <option selected disabled value="0">Open this select menu</option>
+              <option v-for="i in 10" :value="i" :key="i">選購 {{ i }} {{ tempProduct.unit }}</option>
+            </select>
+          </div>
+          <div class="modal-footer">
+            <div class="text-danger text-nowrap mr-3" v-if="0!==tempProduct.count">小計 <strong>{{
+              sumOfTempProduct }}</strong> 元
+            </div>
+            <button type="button" class="btn btn-info" @click.prevent="addToCart(tempProduct)">Add
+              to cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  name: 'HomeProducts',
+  data() {
+    return {
+      categories: [],
+      currentCategory: '',
+      products: [],
+      tempProduct: {},
+    };
+  },
+  methods: {
+    addToCart(product) {
+      if (product.count === 0) return;
+      this.$bus.$emit('add to cart', product);
+      this.$(this.$refs.moreModal).modal('hide');
+    },
+    openModal(id) {
+      this.retrieveProductDetail(id);
+    },
+    applyCategory(item) {
+      if (this.currentCategory === '' || this.currentCategory !== item) {
+        this.currentCategory = item;
+      } else {
+        this.currentCategory = '';
+      }
+    },
+    retrieveProducts(page = 0) {
+      const loader = this.$loading.show({ isFullPage: true });
+      this.$http.get(`${process.env.VUE_APP_API_PATH}/products`, {
+        params: { page },
+      }).then(({ data: { data } }) => {
+        this.categories = new Set(data.map((product) => product.category));
+        this.products = data;
+        loader.hide();
+      });
+    },
+    retrieveProductDetail(id) {
+      const loader = this.$loading.show({
+        isFullPage: false,
+        loader: 'dots',
+        container: this.$refs[`moreDetail${id}`][0],
+      });
+      this.$http.get(`${process.env.VUE_APP_API_PATH}/product/${id}`)
+        .then(({ data: { data } }) => {
+          this.tempProduct = data;
+          this.$set(this.tempProduct, 'count', 0);
+          loader.hide();
+          this.$(this.$refs.moreModal).modal('show');
+        });
+    },
+  },
+  created() {
+    this.retrieveProducts();
+  },
+  computed: {
+    filterProducts() {
+      if (this.currentCategory === '') return this.products;
+      return this.products.filter((item) => this.currentCategory === item.category);
+    },
+    sumOfTempProduct() {
+      return this.tempProduct.price * this.tempProduct.count;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+  .category-block li:hover {
+    background-color: rgba(31, 87, 94, 0.5);
+    a{
+      color: #ffffff;
+    }
+  }
+
+  .category-block li.active {
+    background-color: rgba(31, 87, 94, 0.5);
+    border-style: none;
+    a {
+      color: white;
+    }
+  }
+
+  .category-block li a {
+    text-decoration: none;
+    color: #343a40;
+  }
+</style>
